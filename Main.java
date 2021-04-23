575c42df574381d6b244169984646577b2dbe8ef
RofiLiyanan <82301773+RofiLiyanan@users.noreply.github.com>
2021-04-23 14:50:37 +0700

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,15 @@
+package kuis;
+
+
+public class Main {
+
+   
+    public static void main(String[] args) {
+         Segitiga los = new Segitiga();
+        
+        los.display();
+        los.segitiga();
+    }
+    
+    
+}
